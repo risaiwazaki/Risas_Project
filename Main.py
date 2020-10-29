4264e297ad04d7bf0f5afade25d8e58f6469bbc0
risaiwazaki <72892004+risaiwazaki@users.noreply.github.com>
2020-10-29 15:45:41 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,26 +9,30 @@ app = Flask(__name__)
 #connects default URL of server to a python function
 @app.route('/')
 def home_route():
-    #function use Flask import (Jinga) to render an HTML template
-    return render_template("home1.html",
-                           projects=project.setup())
+  #function use Flask import (Jinga) to render an HTML template
+  return render_template("home1.html",
+  projects=project.setup())
 
 #connects /hello path of server to render hello.html
 @app.route('/hello/')
 def hello_route():
-    return render_template("hello.html",
-                           projects=project.setup())
-
+  return render_template("hello.html", 
+  projects=project.setup())
+    
 #connects /flask path of server to render flask.html
 @app.route('/flask/')
 def flask_route():
-    return render_template("flask.html", projects=project.setup())
+  return render_template("flask.html", projects=project.setup())
 
 
 if __name__ == "__main__":
     #runs the application on the repl development server
     #app.run(debug=True, host='192.168.1.42', port='8080')
     app.run(debug=True, port='3000', host='0.0.0.0')
+
+
+
+
 
 
 
