a04fde31445f42453412ec2cfc1eab498e8d872b
Risa <risaiwazaki55@gmail.com>
2020-11-02 21:21:40 -0800

    fixed background

--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
 import data  # projects definitions are placed in different file
 
-
 # https://flask.palletsprojects.com/en/1.1.x/api/
 from flask import Flask, render_template
 
@@ -25,6 +24,7 @@ def hello_route():
 @app.route('/flask/')
 def flask_route():
     return render_template("flask.html", projects=data.setup())
+
 
 if __name__ == "__main__":
     # runs the application on the repl development server
